3df8c50176411e1a50df1a39b16e4f87786bb6ff
Eddie-Thiiru <eddiewachaga6@gmail.com>
2022-11-03 11:18:06 +0300

    Edit script.js
    Add function that plays a single round and returns the winner.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let randomNumber =  Math.floor(Math.random() * 100) + 1;
 
-const one = "Rock";
-const two = "Paper";
-const three = "Scissors";
+const one = "rock";
+const two = "paper";
+const three = "scissors";
 
 function getComputerChoice() {
 
@@ -13,7 +13,37 @@ function getComputerChoice() {
     } else {
         return three;
     } 
+} 
+
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === one && computerSelection === two) {
+        return "You Loose! Paper beats Rock";
+    } else if (playerSelection === one && computerSelection === three) {
+        return "You Win! Rock beats Scissors";
+    } else if (playerSelection === two && computerSelection === one) {
+        return "You Win! Paper beats Rock";
+    } else if (playerSelection === two && computerSelection === three) {
+        return "You Loose! Scissors beats Paper";
+    } else if (playerSelection === three && computerSelection === one) {
+        return "You Loose! Rock beats Scissors";
+    } else if (playerSelection === three && computerSelection === two) {
+        return "You Win! Scissors beats Paper";
+    } else {
+        return "It's a Draw!";
+    }
 }
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
+
+
+
+
+
+
 
 
 
