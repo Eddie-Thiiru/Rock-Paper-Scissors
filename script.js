050d88a26f2b8d3e03d2269573d7521b9fea2a60
Eddie-Thiiru <eddiewachaga6@gmail.com>
2022-11-07 17:49:58 +0300

    Edit script.js
    Changed the name of the function 'playScore()' to 'game()'. Also changed
    the score increment syntax of the game() function to a shorter syntax.
    
    Added a 'for Loop' to play and keep score for five rounds.

--- script.js
+++ script.js
@@ -1,73 +1,76 @@
-let randomNumber =  Math.floor(Math.random() * 100) + 1;
-
 const one = "rock";
 const two = "paper";
 const three = "scissors";
 
-function getComputerChoice() {
+let playerValue = 0;
+let computerValue = 0;
 
-    if (randomNumber >= 1 && randomNumber <= 33) {
-        return one;
-    } else if (randomNumber >= 34 && randomNumber <= 67) {
-        return two;
-    } else {
-        return three;
+for (let i = 0; i < 5; i++) {
+
+    function getComputerChoice() {
+        
+        if (randomNumber >= 1 && randomNumber <= 33) {
+            return one;
+        } else if (randomNumber >= 34 && randomNumber <= 67) {
+            return two;
+        } else {
+            return three;
+        } 
     } 
-} 
+    let randomNumber =  Math.floor(Math.random() * 100) + 1;
 
-function funcPrompt() {
-    let answer = prompt("Rock, Paper or Scissors?");
-    if (answer === null || answer === "") {
-        alert('Please enter an answer.');
-        funcPrompt();
-        return;
+    function funcPrompt() {
+        let answer = prompt("Rock, Paper or Scissors?");
+        if (answer === null || answer === "") {
+            alert('Please enter an answer.');
+            funcPrompt();
+            return;
+        }
+        answer = answer.toLowerCase();
+        if (answer === one || answer === two || answer === three) {
+            return answer;
+        } else {
+            alert("Please enter correct answer")
+            funcPrompt();
+        }
     }
-    answer = answer.toLowerCase();
-    if (answer === one || answer === two || answer === three) {
-        return answer;
-    } else {
-        alert("Please enter correct answer")
-        funcPrompt();
+
+    function playRound(playerSelection, computerSelection) {
+
+        if (playerSelection === one && computerSelection === two) {
+            return "You Loose This Round!";
+        } else if (playerSelection === one && computerSelection === three) {
+            return "You Win This Round!";
+        } else if (playerSelection === two && computerSelection === one) {
+            return "You Win This Round!";
+        } else if (playerSelection === two && computerSelection === three) {
+            return "You Loose This Round!";
+        } else if (playerSelection === three && computerSelection === one) {
+            return "You Loose This Round!";
+        } else if (playerSelection === three && computerSelection === two) {
+            return "You Win This Round!";
+        }else {
+            return "You Draw This Round!";
+        }  
     }
+    const playerSelection = funcPrompt();
+    const computerSelection = getComputerChoice();
+
+    function game() {
+        let valueCount = playRound(playerSelection, computerSelection);
+
+        if (valueCount === "You Win This Round!") {
+            console.log("You Win This Round!");
+            return ++playerValue;
+        } else if (valueCount === "You Loose This Round!") {
+            console.log("You Loose This Round!");
+            return ++computerValue;
+        }else {
+            console.log("You Draw This Round");
+            return playerValue,
+                    computerValue;
+        }    
+    }game();
 }
-
-function playRound(playerSelection, computerSelection) {
-
-    if (playerSelection === one && computerSelection === two) {
-        return "You Loose This Round!";
-    } else if (playerSelection === one && computerSelection === three) {
-        return "You Win This Round!";
-    } else if (playerSelection === two && computerSelection === one) {
-        return "You Win This Round!";
-    } else if (playerSelection === two && computerSelection === three) {
-        return "You Loose This Round!";
-    } else if (playerSelection === three && computerSelection === one) {
-        return "You Loose This Round!";
-    } else if (playerSelection === three && computerSelection === two) {
-        return "You Win This Round!";
-    }else {
-        return "It's a Draw!";
-    }
-  
-}
-const playerSelection = funcPrompt();
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
-
-
-function playScore() {
-    let valueCount = playRound(playerSelection, computerSelection);
-    let playerValue = 0;
-    let computerValue = 0;
-
-    if (valueCount === "You Win This Round!") {
-        playerValue += 1;
-    } else if (valueCount === "You Loose This Round!") {
-        computerValue += 1;
-    }else {
-        playerValue += 0;
-        computerValue += 0;
-    }  
-}
-const score = playScore();
-
+console.log(playerValue, "player");
+console.log(computerValue, "computer");
