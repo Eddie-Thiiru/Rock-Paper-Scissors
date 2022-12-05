b8e280a9f5d57bec9c6ccb2761d48d5833ed3365
Eddie-Thiiru <eddiewachaga6@gmail.com>
2022-12-05 11:42:13 +0300

    Add div for displaying results
    Modify 'playRound' function to display running score results for each button click using Dom methods.

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
-const one = "rock";
-const two = "paper";
-const three = "scissors";
-
-let playerValue = 0;
-let computerValue = 0;
-
 const buttons = document.querySelectorAll("button");
+
+const content = document.createElement("div");
+content.classList.toggle("results");
+
+let playerScore = 0;
+let computerScore = 0;
+
 
 function getPlayerChoice(e){
     if(e.target.value === "rock") {
-        return one;
+        return "rock";
     }else if(e.target.value === "paper") {
-        return two;
+        return "paper";
     }else {
-        return three;
+        return "scissors";
     }
 }
 
-    function getComputerChoice() {
-        
-        if (randomNumber >= 1 && randomNumber <= 33) {
-            return one;
-        } else if (randomNumber >= 34 && randomNumber <= 67) {
-            return two;
-        } else {
-            return three;
-        } 
+function getComputerChoice() {
+    const randomNumber =  Math.floor(Math.random() * 100) + 1;
+
+    if (randomNumber >= 1 && randomNumber <= 33) {
+        return "rock";
+    } else if (randomNumber >= 34 && randomNumber <= 67) {
+        return "paper";
+    } else {
+        return "scissors";
     } 
-    let randomNumber =  Math.floor(Math.random() * 100) + 1;
+} 
+    
 
 
     function playRound(e) {
@@ -35,24 +36,32 @@ function getPlayerChoice(e){
         console.log(playerSelection);
         const computerSelection = getComputerChoice();
         console.log(computerSelection)
-
-        if (playerSelection === one && computerSelection === two) {
-            return "You Loose This Round!";
-        } else if (playerSelection === one && computerSelection === three) {
-            return "You Win This Round!";
-        } else if (playerSelection === two && computerSelection === one) {
-            return "You Win This Round!";
-        } else if (playerSelection === two && computerSelection === three) {
-            return "You Loose This Round!";
-        } else if (playerSelection === three && computerSelection === one) {
-            return "You Loose This Round!";
-        } else if (playerSelection === three && computerSelection === two) {
-            return "You Win This Round!";
+        if (playerSelection === "rock" && computerSelection === "paper") {
+            computerScore += 1;
+            content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
+        } else if (playerSelection === "rock" && computerSelection === "scissors") {
+            playerScore += 1;
+            content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
+        } else if (playerSelection === "paper" && computerSelection === "rock") {
+            playerScore += 1;
+            content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
+        } else if (playerSelection === "paper" && computerSelection === "scissors") {
+            computerScore += 1;
+            content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
+        } else if (playerSelection === "scissors" && computerSelection === "rock") {
+            computerScore += 1;
+            content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
+        } else if (playerSelection === "scissors" && computerSelection === "paper") {
+            playerScore += 1;
+            content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
         }else {
-            return "You Draw This Round!";
-        }  
+            playerScore += 0;
+            computerScore += 0;
+            content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
+        }
     }
 
+    container.appendChild(content);
 
 buttons.forEach(button => button.addEventListener('click', playRound));
 
