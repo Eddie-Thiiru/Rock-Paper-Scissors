67d1cd5793d0d4ba8188f79ac03161b91a538800
Eddie-Thiiru <eddiewachaga6@gmail.com>
2022-12-05 11:51:15 +0300

    Add node for displaying winner
    Modify 'playRound' function to display the winner once one player reaches 5 points.

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const buttons = document.querySelectorAll("button");
+const container = document.querySelector("#container");
 
 const content = document.createElement("div");
 content.classList.toggle("results");
 
+const winner = document.createElement("div");
+content.classList.toggle("winner");
+
 let playerScore = 0;
 let computerScore = 0;
-
 
 function getPlayerChoice(e){
     if(e.target.value === "rock") {
@@ -28,41 +31,54 @@ function getComputerChoice() {
         return "scissors";
     } 
 } 
-    
 
-
-    function playRound(e) {
-        const playerSelection = getPlayerChoice(e);
-        console.log(playerSelection);
-        const computerSelection = getComputerChoice();
-        console.log(computerSelection)
-        if (playerSelection === "rock" && computerSelection === "paper") {
-            computerScore += 1;
-            content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
-        } else if (playerSelection === "rock" && computerSelection === "scissors") {
-            playerScore += 1;
-            content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
-        } else if (playerSelection === "paper" && computerSelection === "rock") {
-            playerScore += 1;
-            content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
-        } else if (playerSelection === "paper" && computerSelection === "scissors") {
-            computerScore += 1;
-            content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
-        } else if (playerSelection === "scissors" && computerSelection === "rock") {
-            computerScore += 1;
-            content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
-        } else if (playerSelection === "scissors" && computerSelection === "paper") {
-            playerScore += 1;
-            content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
-        }else {
-            playerScore += 0;
-            computerScore += 0;
-            content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
-        }
+function playRound(e) {
+    winner.textContent = "";
+    const playerSelection = getPlayerChoice(e);
+    console.log(playerSelection);
+    const computerSelection = getComputerChoice();
+    console.log(computerSelection)
+ 
+    if (playerSelection === "rock" && computerSelection === "paper") {
+        computerScore += 1;
+        content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        playerScore += 1;
+        content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        playerScore += 1;
+        content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        computerScore += 1;
+        content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        computerScore += 1;
+        content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        playerScore += 1;
+        content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
+    }else {
+        playerScore += 0;
+        computerScore += 0;
+        content.textContent = "Player:" + playerScore + " Computer:" + computerScore;
     }
 
-    container.appendChild(content);
+    if (playerScore === 5) {
+        winner.textContent = "Hurray! five points for the Win!";
+        playerScore *= 0;
+        computerScore *= 0;
+    }else if (computerScore === 5) {
+        winner.textContent = "You Lost! Better luck next time";
+        playerScore *= 0;
+        computerScore *= 0;
+    }
+    console.log(playerScore);
+    console.log(computerScore);  
+}
 
+container.appendChild(content);
+container.appendChild(winner);
 buttons.forEach(button => button.addEventListener('click', playRound));
+
 
  
